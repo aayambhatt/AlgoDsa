2393b25a278dc8362563532b8872652860ed6405
aayambhatt <aayambhatt23@gmail.com>
2026-01-30 19:39:35 +0530

    Data types

---
+++ src/A_basics/A_languageBasics/DataTypes.java
@@ -0,0 +1,26 @@
+package A_basics.A_languageBasics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+        // -128 to 127
+        takes 8 bits in the memory
+         */
+        byte byteNum = 125;
+        System.out.println(byteNum);
+
+        /*
+        -2^31 to 2^31-1
+        32 bits
+         */
+        int num = 12452;
+
+        long longNum = 1253412;
+
+        double doubleNum = 5.4;
+
+        char ch = 'a';
+
+
+    }
+}
