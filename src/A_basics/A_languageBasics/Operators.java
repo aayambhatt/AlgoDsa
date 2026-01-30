828ed8cd3a4456092c1da1c75f1bd044eceef658
aayambhatt <aayambhatt23@gmail.com>
2026-01-30 19:40:37 +0530

    Java journey

---
+++ src/A_basics/A_languageBasics/Operators.java
@@ -0,0 +1,30 @@
+package A_basics.A_languageBasics;
+
+public class Operators {
+    public static void main(String[] args) {
+        int num = 5;
+        int secondNum = 7;
+        int theirMultiply = num*secondNum;
+        System.out.println(theirMultiply);
+
+        int x = 10;
+        int y = 3;
+        int rem = x%y;
+        System.out.println(rem);
+
+        int num1 = 5;
+        int num2 = +num1;
+        System.out.println(num2);
+        int num3 = -num1;
+        System.out.println(num3);
+
+        boolean fl1 = true;
+        boolean fl2 = !fl1;
+        System.out.println(fl2);
+        boolean fl3 = !!fl1;
+        System.out.println(fl3);
+
+
+
+    }
+}
