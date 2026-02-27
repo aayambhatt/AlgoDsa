5d935157d1152bf9bb2aa245d8727dea94c4b2df
aayambhatt <aayambhatt23@gmail.com>
2026-02-27 23:34:47 +0530

    Count all odd digits in a number

---
+++ src/A_basics/E_conceptsBasics/A_basicMaths/CountAllOddDigits.java
@@ -0,0 +1,21 @@
+package A_basics.E_conceptsBasics.A_basicMaths;
+
+public class CountAllOddDigits {
+    public static void main(String[] args) {
+        System.out.println( countAllOddDigits(15));
+    }
+
+    public static int countAllOddDigits(int n){
+        int count = 0;
+
+        while(n>0){
+            int ld = n % 10;
+            if(ld%2==1){
+                count++;
+            }
+            n = n/10;
+        }
+
+        return count;
+    }
+}
