ec629a92c2abd5976ca9c4fdb08239f9b733185f
aayambhatt <aayambhatt23@gmail.com>
2026-02-27 23:34:16 +0530

    Count all digits

---
+++ src/A_basics/E_conceptsBasics/A_basicMaths/CountAllDigitsInANumber.java
@@ -0,0 +1,17 @@
+package A_basics.E_conceptsBasics.A_basicMaths;
+
+public class CountAllDigitsInANumber {
+    public static void main(String[] args) {
+        System.out.println(CountDigits(24));
+    }
+
+    public static int CountDigits(int n) {
+        if (n == 0) return 1;
+        int count = 0;
+        while (n > 0) {
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+}
